72ad12490a972a4bf6906a49cef8c6225ee29c7e
Edwin van de Pol <developer@edwinvandepol.nl>
2024-04-17 15:16:11 +0200

    Merge devices and drivers

--- drivers/sensor/driver.js
+++ drivers/sensor/driver.js
@@ -1,44 +1,8 @@
 'use strict';
 
 const Driver = require('../../lib/Driver');
-const { blank } = require('../../lib/Utils');
-const { DeviceType } = require('../../lib/Enums');
 
 class SensorDriver extends Driver {
-
-  /*
-  | Pairing functions
-  */
-
-  // Return devices while pairing
-  async getPairDevices({ oAuth2Client }) {
-    return oAuth2Client.getDevices(DeviceType.sensor);
-  }
-
-  // Return data to create the device
-  getDeviceData(device) {
-    if (blank(device.location)) return {};
-
-    const data = {
-      name: device.location.name,
-      data: {
-        id: device.id,
-      },
-      settings: {
-        product_name: device.product,
-        away_mode: device.location.away_mode,
-      },
-      store: {
-        location_id: device.location.id,
-        timezone: device.location.tz,
-      },
-    };
-
-    this.log('Device found', JSON.stringify(data));
-
-    return data;
-  }
-
 }
 
 module.exports = SensorDriver;
