a86475877c2b2fc3dfd07f46c2c3130ed0b74148
Edwin van de Pol <developer@edwinvandepol.nl>
2024-09-18 02:49:51 +0200

    Updated client and added model

---
+++ models/Sensor.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const { filled } = require('../lib/Utils');
+
+class Sensor {
+
+  /**
+   * Represents a beam.
+   *
+   * @constructor
+   */
+  constructor(data) {
+    this.id = data.id;
+    this.battery_level = data.battery_level?.toLowerCase();
+    this.connected = data.connected;
+    this.product = data.product;
+    this.location = data.location;
+  }
+
+  /**
+   * Return device capability values.
+   *
+   * @return {Object}
+   */
+  get capabilityValues() {
+    if (!this.valid) return {};
+
+    return Object.fromEntries(Object.entries({
+      away_mode: this.location?.away_mode || null,
+      battery_level: this.battery_level || 'unknown',
+      connected: this.connected || null,
+    }).filter(([_, v]) => v));
+  }
+
+  /**
+   * Return device data.
+   *
+   * @return {Object}
+   */
+  get data() {
+    if (!this.valid) return {};
+
+    return {
+      name: this.location?.name,
+      data: {
+        id: this.id,
+      },
+      settings: this.settings,
+      store: this.store,
+    };
+  }
+
+  /**
+   * Return device settings.
+   *
+   * @return {{
+   *  product_name: string,
+   *  away_mode: boolean
+   * }}
+   */
+  get settings() {
+    return {
+      away_mode: this.location?.away_mode || false,
+      product_name: this.product || '-',
+    };
+  }
+
+  /**
+   * Return device store.
+   *
+   * @return {{
+   *  location_id: string,
+   *  timezone: string
+   * }}
+   */
+  get store() {
+    return {
+      location_id: this.location?.id || '-',
+      timezone: this.location?.tz || '-',
+    };
+  }
+
+  /**
+   * Return whether device is valid.
+   *
+   * @return {boolean}
+   */
+  get valid() {
+    return filled(this.id) && filled(this.location);
+  }
+
+}
+
+module.exports = Sensor;
